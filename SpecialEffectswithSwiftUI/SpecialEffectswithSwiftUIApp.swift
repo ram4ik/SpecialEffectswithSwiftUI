5185f3eb606328d7bee49f3b94b2e58ed6b5b097
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-01-29 16:03:16 +0200

    Initial Commit

---
+++ SpecialEffectswithSwiftUI/SpecialEffectswithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialEffectswithSwiftUIApp.swift
+//  SpecialEffectswithSwiftUI
+//
+//  Created by ramil on 29.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpecialEffectswithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
